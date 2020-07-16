83630dcd0da2df39e114b990c952b6c0b9eeeef3
NingrumFadillah <>
2020-07-16 13:19:01 +0700

    Bisa run file python

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,68 @@
+
+<div id="sidebar-nav" class="sidebar">
+			<div class="sidebar-scroll">
+				<nav>
+					@if(Auth::user()->role=='admin')
+					 <br \>
+					 <br \>
+					 <br \>
+
+					<ul class="nav">
+						<li>
+							<a href="#" class="active"><i class="lnr lnr-user"></i> 
+								<span>{{ Auth::user()->role }} </span>
+							</a>
+						</li>
+						<li>
+							<a href="{{ url('pengguna') }}" class=""><i class="lnr lnr-home"></i> 
+								<span>Pengguna</span>
+							</a>
+						</li>
+						<li>
+							<a href="{{ url('riwayat') }}" ><i class="lnr lnr-chart-bars"></i> 
+								<span>Riwayat</span>
+							</a>
+						</li>
+						<li>
+							<a href="{{ url('tes') }}" ><i class="lnr lnr-chart-bars"></i> 
+								<span>Tes</span>
+							</a>
+						</li>
+						<li>
+							<a href="{{ route('logout') }}" ><i class="lnr lnr-arrow-left-circle"></i> 
+								<span>Logout</span>
+							</a>
+						</li>
+					</ul>
+					@else
+					 <br \>
+					 <br \>
+					 <br \>
+					
+						<ul class="nav">
+						<li>
+							<a href="#" class="active"><i class="lnr lnr-user"></i> 
+								<span>{{ Auth::user()->role }} </span>
+							</a>
+						</li>
+
+						<li>
+							<a href="{{ url('dashboard') }}" class=""><i class="lnr lnr-home"></i> 
+								<span>Beranda</span>
+							</a>
+						</li>
+						<li>
+							<a href="{{ url('cekmutasiku') }}" ><i class="lnr lnr-chart-bars"></i> 
+								<span>Cek Mutasi</span>
+							</a>
+						</li>
+						<li>
+							<a href="{{ route('logout') }}" ><i class="lnr lnr-arrow-left-circle"></i> 
+								<span>Logout</span>
+							</a>
+						</li>
+					</ul>
+					@endif
+				</nav>
+			</div>
+		</div>
